8a1e820291c074d981d00617dce33face9cc634e
AlAminAshik <alaminashik890.bd@gmail.com>
2025-01-23 13:00:54 +0600

    initial commit and code

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <NewPing.h>
+#include <Arduino.h>
+
+#define TRIG_F 12  //D6 //2 
+#define MAX_DISTANCE_FRONT ddf
+#define ddf 184  // detection distance front
+
+NewPing sonarF(TRIG_F, TRIG_F, MAX_DISTANCE_FRONT + 30);
+
+int dF;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+long dst_median(NewPing s, int max, int cnt) {
+  long tmr = millis();
+  long dstt = 1;
+  for (int i = 0; i < cnt; i++) {
+    long d = s.ping_cm();
+    //dstt += s.ping_cm();
+    if (d == 0) {
+      d = max;
+    }
+    dstt = dstt * d;
+  }
+  //dstt = dstt / cnt;
+  dstt = round(pow(dstt, 1.0 / cnt));
+  Serial.println(millis() - tmr);
+  return dstt;
+}
+
+void getSensorData() {
+  dF = dst_median(sonarF, MAX_DISTANCE_FRONT + 30, 4);
+  delay(10);
+  Serial.println("Front: " + String(dF) + "cm");
+}
+
+void loop(){
+  getSensorData();
+  delay(1000);
+}
