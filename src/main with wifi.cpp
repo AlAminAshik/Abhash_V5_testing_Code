703ce1e281581379512a7f594a2083e24333d361
AlAminAshik <106368872+AlAminAshik@users.noreply.github.com>
2025-02-09 10:53:37 +0600

    3 sensor added to offline code

--- src/main with wifi.cpp
+++ src/main with wifi.cpp
@@ -1,74 +1,79 @@
-#include <NewPing.h>
-#include <Arduino.h>
-#include <WiFi.h>
+// #include <NewPing.h>
+// #include <Arduino.h>
+// #include <WiFi.h>
 
-const char *ssid     = "Alamin";    //enter name of wifi
-const char *password = "12345678";      //enter password for wifi
-const char* host = "www.google.com";    //website to check for connectivity
-WiFiClient client;
-// 59 89 103
-//#define TRIG_F 12  //D6 //2  //wemos d1 mini
-#define TRIG_F 4   //D4 //26 //nodemcu esp32
+// const char *ssid     = "Alamin";    //enter name of wifi
+// const char *password = "12345678";      //enter password for wifi
+// const char* host = "www.google.com";    //website to check for connectivity
+// WiFiClient client;
+// // 59 89 103
 
-#define MAX_DISTANCE_FRONT 184
+// #define TRIG_L 16  //pin 27
+// #define TRIG_F 4  //pin 26 
+// #define TRIG_R 17  //pin 28
 
-NewPing sonarF(TRIG_F, TRIG_F, MAX_DISTANCE_FRONT);
+// #define MAX_DISTANCE_FRONT 184
+// #define MAX_DISTANCE_SIDE 184
 
-int dF;
+// NewPing sonarL(TRIG_L, TRIG_L, MAX_DISTANCE_SIDE + 30);
+// NewPing sonarF(TRIG_F, TRIG_F, MAX_DISTANCE_FRONT + 30);
+// NewPing sonarR(TRIG_R, TRIG_R, MAX_DISTANCE_SIDE + 30);
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Start");
-  WiFi.begin(ssid, password);
-  pinMode(2, OUTPUT); //on board led  
-}
+// int dL, dF, dR;
 
-long dst_median(NewPing s, int max, int cnt) {
-  long tmr = millis();
-  long dstt = 1;
-  for (int i = 0; i < cnt; i++) {
-    long d = s.ping_cm();
-    //dstt += s.ping_cm();
-    if (d == 0) {
-      d = max;
-    }
-    dstt = dstt * d;
-  }
-  //dstt = dstt / cnt;
-  dstt = round(pow(dstt, 1.0 / cnt));
-  Serial.println(millis() - tmr);
-  return dstt;
-}
+// void setup() {
+//   Serial.begin(115200);
+//   Serial.println("Start");
+//   WiFi.begin(ssid, password);
+//   pinMode(2, OUTPUT); //on board led  
+// }
 
-void getSensorData() {
-  dF = dst_median(sonarF, MAX_DISTANCE_FRONT, 4);
-  delay(10);
-  Serial.println("Front: " + String(dF) + "cm");
-}
+// long dst_median(NewPing s, int max, int cnt) {
+//   long tmr = millis();
+//   long dstt = 1;
+//   for (int i = 0; i < cnt; i++) {
+//     long d = s.ping_cm();
+//     //dstt += s.ping_cm();
+//     if (d == 0) {
+//       d = max;
+//     }
+//     dstt = dstt * d;
+//   }
+//   //dstt = dstt / cnt;
+//   dstt = round(pow(dstt, 1.0 / cnt));
+//   Serial.println(millis() - tmr);
+//   return dstt;
+// }
 
-void loop(){
-  int counting = 0;             //number of times code tried to connect with wifi
-  while(WiFi.status() != WL_CONNECTED && counting <=5){ //untill wifi is connected and counting is less than 5, keep trying to connect
-    Serial.println("Disconnected");
-    delay(500);
-    counting++;
-    digitalWrite(2, HIGH);     //keep off on board led if not connected to net
-  }
-  if(WiFi.status() == WL_CONNECTED){
-    digitalWrite(2,LOW);   //glow on board led when connection is successful
-  }
+// void getSensorData() {
+//   dF = dst_median(sonarF, MAX_DISTANCE_FRONT, 4);
+//   delay(10);
+//   Serial.println("Front: " + String(dF) + "cm");
+// }
+
+// void loop(){
+//   int counting = 0;             //number of times code tried to connect with wifi
+//   while(WiFi.status() != WL_CONNECTED && counting <=5){ //untill wifi is connected and counting is less than 5, keep trying to connect
+//     Serial.println("Disconnected");
+//     delay(500);
+//     counting++;
+//     digitalWrite(2, HIGH);     //keep off on board led if not connected to net
+//   }
+//   if(WiFi.status() == WL_CONNECTED){
+//     digitalWrite(2,LOW);   //glow on board led when connection is successful
+//   }
   
-  Serial.print("wifi strength: ");
-  Serial.println(WiFi.RSSI());  //show signal strength, positive if not connected, negative if connected, more negative if far away. doesnt show net connection
+//   Serial.print("wifi strength: ");
+//   Serial.println(WiFi.RSSI());  //show signal strength, positive if not connected, negative if connected, more negative if far away. doesnt show net connection
   
-  if(client.connect(host,80)){
-    Serial.println("internet is available");
-    client.stop();
-    }
-  else{
-    Serial.println("Internet is not available");
-  }
+//   if(client.connect(host,80)){
+//     Serial.println("internet is available");
+//     client.stop();
+//     }
+//   else{
+//     Serial.println("Internet is not available");
+//   }
   
-  getSensorData();
-  delay(1000);
-}
+//   getSensorData();
+//   delay(1000);
+// }
--- src/main without wifi.cpp
+++ src/main without wifi.cpp
@@ -2,13 +2,18 @@
 #include <Arduino.h>
 
 //#define TRIG_F 12  //D6 //2  //wemos d1 mini
-#define TRIG_F 4   //D4 //26 //nodemcu esp32
+#define TRIG_L 16  //pin 27
+#define TRIG_F 4  //pin 26 
+#define TRIG_R 17  //pin 28
 
 #define MAX_DISTANCE_FRONT 184
+#define MAX_DISTANCE_SIDE 184
 
-NewPing sonarF(TRIG_F, TRIG_F, MAX_DISTANCE_FRONT);
+NewPing sonarL(TRIG_L, TRIG_L, MAX_DISTANCE_SIDE + 30);
+NewPing sonarF(TRIG_F, TRIG_F, MAX_DISTANCE_FRONT + 30);
+NewPing sonarR(TRIG_R, TRIG_R, MAX_DISTANCE_SIDE + 30);
 
-int dF;
+int dL, dF, dR;
 
 void setup() {
   Serial.begin(115200);
@@ -33,9 +38,13 @@ long dst_median(NewPing s, int max, int cnt) {
 }
 
 void getSensorData() {
-  dF = dst_median(sonarF, MAX_DISTANCE_FRONT, 4);
+  dF = dst_median(sonarF, MAX_DISTANCE_FRONT + 30, 4);
   delay(10);
-  Serial.println("Front: " + String(dF) + "cm");
+  dL = dst_median(sonarL, MAX_DISTANCE_SIDE + 30, 4);
+  delay(10);
+  dR = dst_median(sonarR, MAX_DISTANCE_SIDE + 30, 4);
+  delay(10);
+  Serial.println("Distance: Left: " + String(dL) + "cm - Front: " + String(dF) + "cm - Right: " + String(dR) + "cm");
 }
 
 void loop(){
