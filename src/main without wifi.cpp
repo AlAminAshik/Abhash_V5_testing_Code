b5390ffa887de6b199d83e5c0aed98d7224ab2b4
AlAminAshik <106368872+AlAminAshik@users.noreply.github.com>
2025-02-09 11:42:34 +0600

    push button on/off added

--- src/main without wifi.cpp
+++ src/main without wifi.cpp
@@ -6,18 +6,30 @@
 #define TRIG_F 4  //pin 26 
 #define TRIG_R 17  //pin 28
 
+const int CH_PD_PIN = 0;          // pin 25 GPIO 0
+const int PWR_BUTTON_PIN = 19;    // pin 31
+
+unsigned long previousMillis_PWR_Butt = 0;  // will store last time button has pressed
+
 #define MAX_DISTANCE_FRONT 184
 #define MAX_DISTANCE_SIDE 184
-
 NewPing sonarL(TRIG_L, TRIG_L, MAX_DISTANCE_SIDE + 30);
 NewPing sonarF(TRIG_F, TRIG_F, MAX_DISTANCE_FRONT + 30);
 NewPing sonarR(TRIG_R, TRIG_R, MAX_DISTANCE_SIDE + 30);
-
 int dL, dF, dR;
+
+
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Start");
+
+  //Power on schedule
+  pinMode(PWR_BUTTON_PIN, INPUT_PULLUP);  // Button with pull-up
+  pinMode(CH_PD_PIN, OUTPUT);         // sets GPIO0 as an output which will pull the mosfet ON
+  digitalWrite(CH_PD_PIN, HIGH);      // keep the mosfet off
+  delay(4000);                        // wait for a defined delay time the device to boot up
+  digitalWrite(CH_PD_PIN, LOW);       // latches the transistor to keep device on
 }
 
 long dst_median(NewPing s, int max, int cnt) {
@@ -48,6 +60,20 @@ void getSensorData() {
 }
 
 void loop(){
+  // Power off schedule
+  if(digitalRead(PWR_BUTTON_PIN) == LOW) {  //if button is pressed when device is active
+    unsigned long currentMillis = millis(); // get the current time, so we can compare it with the previous time
+    if(currentMillis - previousMillis_PWR_Butt > 5000) { //if button pressed for 5 seconds
+      while(digitalRead(PWR_BUTTON_PIN) == LOW){    //while button is kept pressed
+        //sound power down
+      }
+      digitalWrite(CH_PD_PIN, HIGH);            // turns the mosfet off and thus device off
+    }
+  }
+  else{ 
+    previousMillis_PWR_Butt = millis();  //if button is not pressed, store the current time
+  }
+
   getSensorData();
   delay(1000);
 }
