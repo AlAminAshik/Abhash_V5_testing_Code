28e3f0bd0bca06f7af95a9a15219ce2b7c7ba457
AlAminAshik <alaminashik890.bd@gmail.com>
2025-02-20 21:44:19 +0600

    all voices added with successful compile

--- src/main with wifi.cpp
+++ src/main with wifi.cpp
@@ -5,17 +5,29 @@
 // #include "driver/adc.h" //OR 
 #include "esp_task_wdt.h" //for disabling watchdog timer
 
-#include "SoundData.h"
-#include "Audio_Power_On.h"
-#include "Audio_Power_Off.h"
+#include "Audio_agate_thakun.h"
+#include "Audio_Bame_Badha.h"
+#include "Audio_Bame_Jan.h"
+#include "Audio_Dane_Badha.h"
+#include "Audio_Dane_Jan.h"
 #include "Audio_Halka_Dane_Jan.h"
+#include "Audio_Power_Off.h"
+#include "Audio_Power_On.h"
+#include "Audio_Thamun.h"
+#include "SoundData.h"
 #include "XT_DAC_Audio.h"
 
+XT_Wav_Class Agate_Thakun_Audio(Agate_Thakun);
+XT_Wav_Class Bame_Badha_Audio(Bame_Badha);
+XT_Wav_Class Bame_Jan_Audio(Bame_Jan);
+XT_Wav_Class Dane_Badha_Audio(Dane_Badha);
+XT_Wav_Class Dane_Jan_Audio(Dane_Jan);
 XT_Wav_Class Halka_Dane_Audio(Halka_Dane_Jan);
-XT_Wav_Class Power_On_Audio(Power_On);
 XT_Wav_Class Power_Off_Audio(Power_Off);
-XT_Wav_Class Sample_audio(sample);   //initializing the sample audio data
-XT_DAC_Audio_Class DacAudio(25, 0); //connected to pin10 or GPIO25 or DAC1
+XT_Wav_Class Power_On_Audio(Power_On);
+XT_Wav_Class Thamu_Audio(Thamun);
+//XT_Wav_Class Sample_audio(sample);   //initializing the sample audio data
+XT_DAC_Audio_Class DacAudio(25, 0); //connected to pin10 or GPIO25 or DAC1 (create the main player class object)
 
 const char *ssid     = "Alamin";    //enter name of wifi
 const char *password = "12345678";      //enter password for wifi
@@ -51,42 +63,68 @@ NewPing sonarR(TRIG_R, TRIG_R, MAX_DISTANCE_SIDE + 30);
 
 int dL, dF, dR;
 
-void Play_Sample()
+
+void Play_Agate_Thakun()
 {
   DacAudio.FillBuffer();
-  DacAudio.Play(&Sample_audio);
-  while (Sample_audio.Playing)
-  {
-    DacAudio.FillBuffer();
-  }
+  DacAudio.Play(&Agate_Thakun_Audio);
+  while (Agate_Thakun_Audio.Playing) DacAudio.FillBuffer();
 }
-void Play_Halka_Dane()
+void Play_Bame_Badha()
+{
+  DacAudio.FillBuffer();
+  DacAudio.Play(&Bame_Badha_Audio);
+  while (Bame_Badha_Audio.Playing) DacAudio.FillBuffer();
+}
+void Play_Bame_Jan()
+{
+  DacAudio.FillBuffer();
+  DacAudio.Play(&Bame_Jan_Audio);
+  while (Bame_Jan_Audio.Playing) DacAudio.FillBuffer();
+}
+void Play_Dane_Badha()
+{
+  DacAudio.FillBuffer();
+  DacAudio.Play(&Dane_Badha_Audio);
+  while (Dane_Badha_Audio.Playing) DacAudio.FillBuffer();
+}
+void Play_Dane_Jan()
+{
+  DacAudio.FillBuffer();
+  DacAudio.Play(&Dane_Jan_Audio);
+  while (Dane_Jan_Audio.Playing) DacAudio.FillBuffer();
+}
+void Play_Halka_Dane_Jan()
 {
   DacAudio.FillBuffer();
   DacAudio.Play(&Halka_Dane_Audio);
-  while (Halka_Dane_Audio.Playing)
-  {
-    DacAudio.FillBuffer();
-  }
+  while (Halka_Dane_Audio.Playing) DacAudio.FillBuffer();
 }
 void Play_Power_Off()
 {
   DacAudio.FillBuffer();
   DacAudio.Play(&Power_Off_Audio);
-  while (Power_Off_Audio.Playing)
-  {
-    DacAudio.FillBuffer();
-  }
+  while (Power_Off_Audio.Playing) DacAudio.FillBuffer();
 }
 void Play_Power_On()
 {
   DacAudio.FillBuffer();
   DacAudio.Play(&Power_On_Audio);
-  while (Power_On_Audio.Playing)
-  {
-    DacAudio.FillBuffer();
-  }
+  while (Power_On_Audio.Playing) DacAudio.FillBuffer();
 }
+void Play_Thamun()
+{
+  DacAudio.FillBuffer();
+  DacAudio.Play(&Thamu_Audio);
+  while (Thamu_Audio.Playing) DacAudio.FillBuffer();
+}
+// void Play_Sample()
+// {
+//   DacAudio.FillBuffer();
+//   DacAudio.Play(&Sample_audio);
+//   while (Sample_audio.Playing) DacAudio.FillBuffer();
+// }
+
 
 void setup() {
   esp_task_wdt_deinit(); // Disable watchdog timer
@@ -222,7 +260,7 @@ void loop(){
     
     //playing sound
     Serial.println("Playing sample");
-    Play_Sample();
+    Play_Thamun();
   }
 
   //check if top button is pressed
@@ -236,7 +274,15 @@ void loop(){
 
     //playing sound
     Serial.println("Playing sample");
-    Play_Halka_Dane();
+    Play_Agate_Thakun();
+    Play_Bame_Badha();
+    Play_Bame_Jan();
+    Play_Dane_Badha();
+    Play_Dane_Jan();
+    Play_Halka_Dane_Jan();
+    Play_Power_Off();
+    Play_Power_On();
+    Play_Thamun();
   }
 
   //check battery voltage
