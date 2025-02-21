39657cbed424b9bf12023d916cd0809c3fbc4b76
AlAminAshik <alaminashik890.bd@gmail.com>
2025-02-21 10:12:56 +0600

    all audio issues fixed

--- src/main_headphone.cpp
+++ src/main_headphone.cpp
@@ -2,28 +2,28 @@
 #include "driver/adc.h"   //analog input driver for voltage range
 
 // //Adding audio files and library
-#include "Audio_agate_thakun.h"         //problem
+#include "Audio_agate_thakun.h"         //D
 #include "Audio_Bame_Badha.h"           //D
-//#include "Audio_Bame_Jan.h"           //problem
+#include "Audio_Bame_Jan.h"           //problem
 #include "Audio_Dane_Badha.h"           //D
-//#include "Audio_Dane_Jan.h"           //problem
+#include "Audio_Dane_Jan.h"           //problem
 #include "Audio_Halka_Dane_Jan.h"       //D
 #include "Audio_Power_Off.h"            //D
 #include "Audio_Power_On.h"             //D
-//#include "Audio_Thamun.h"             //problem
+#include "Audio_Thamun.h"             //problem
 //#include "SoundData.h"
 #include "XT_DAC_Audio.h"
 
 // //initializing and assigning class to the audio array
 XT_Wav_Class Agate_Thakun_Audio(Agate_Thakun);
 XT_Wav_Class Bame_Badha_Audio(Bame_Badha);
-//XT_Wav_Class Bame_Jan_Audio(Bame_Jan);
+XT_Wav_Class Bame_Jan_Audio(Bame_Jan);
 XT_Wav_Class Dane_Badha_Audio(Dane_Badha);
-//XT_Wav_Class Dane_Jan_Audio(Dane_Jan);
+XT_Wav_Class Dane_Jan_Audio(Dane_Jan);
  XT_Wav_Class Halka_Dane_Audio(Halka_Dane_Jan);
 XT_Wav_Class Power_Off_Audio(Power_Off);
 XT_Wav_Class Power_On_Audio(Power_On);
-//XT_Wav_Class Thamu_Audio(Thamun);
+XT_Wav_Class Thamu_Audio(Thamun);
 // XT_Wav_Class Sample_audio(sample);   //initializing the sample audio data
 XT_DAC_Audio_Class DacAudio(25, 0); //connected to pin10 or GPIO25 or DAC1 (create the main player class object)
 
@@ -39,24 +39,24 @@ void Play_Bame_Badha()
   DacAudio.Play(&Bame_Badha_Audio);
   while (Bame_Badha_Audio.Playing) DacAudio.FillBuffer();
 }
-// void Play_Bame_Jan()
-// {
-//   DacAudio.FillBuffer();
-//   DacAudio.Play(&Bame_Jan_Audio);
-//   while (Bame_Jan_Audio.Playing) DacAudio.FillBuffer();
-// }
+void Play_Bame_Jan()
+{
+  DacAudio.FillBuffer();
+  DacAudio.Play(&Bame_Jan_Audio);
+  while (Bame_Jan_Audio.Playing) DacAudio.FillBuffer();
+}
 void Play_Dane_Badha()
 {
   DacAudio.FillBuffer();
   DacAudio.Play(&Dane_Badha_Audio);
   while (Dane_Badha_Audio.Playing) DacAudio.FillBuffer();
 }
-// void Play_Dane_Jan()
-// {
-//   DacAudio.FillBuffer();
-//   DacAudio.Play(&Dane_Jan_Audio);
-//   while (Dane_Jan_Audio.Playing) DacAudio.FillBuffer();
-// }
+void Play_Dane_Jan()
+{
+  DacAudio.FillBuffer();
+  DacAudio.Play(&Dane_Jan_Audio);
+  while (Dane_Jan_Audio.Playing) DacAudio.FillBuffer();
+}
 void Play_Halka_Dane_Jan()
 {
   DacAudio.FillBuffer();
@@ -75,12 +75,12 @@ void Play_Power_On()
   DacAudio.Play(&Power_On_Audio);
   while (Power_On_Audio.Playing) DacAudio.FillBuffer();
 }
-// void Play_Thamun()
-// {
-//   DacAudio.FillBuffer();
-//   DacAudio.Play(&Thamu_Audio);
-//   while (Thamu_Audio.Playing) DacAudio.FillBuffer();
-// }
+void Play_Thamun()
+{
+  DacAudio.FillBuffer();
+  DacAudio.Play(&Thamu_Audio);
+  while (Thamu_Audio.Playing) DacAudio.FillBuffer();
+}
 // void Play_Sample()
 // {
 //   DacAudio.FillBuffer();
@@ -99,11 +99,11 @@ void loop(){
     Serial.println("Playing audio");
     Play_Agate_Thakun();
     Play_Bame_Badha();
-    // Play_Bame_Jan();
-     Play_Dane_Badha();
-    // Play_Dane_Jan();
-     Play_Halka_Dane_Jan();
+    Play_Bame_Jan();
+    Play_Dane_Badha();
+    Play_Dane_Jan();
+    Play_Halka_Dane_Jan();
     Play_Power_Off();
     Play_Power_On();
-    //Play_Thamun();
+    Play_Thamun();
   }
